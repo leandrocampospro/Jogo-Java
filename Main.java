9882555628100fba643e00b181b43e7b4686f7f1
Leandro Campos <leandrocampos659@gmail.com>
2026-09-09 14:13:44 -0300

    feat: inicia projeto Jogo Java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("              JOGOJAVA");
+        System.out.println("================================");
+        System.out.println("Bem-vindo ao jogo!");
+        System.out.println("Jogo criado por Leandro Campos");
+
+
+    }
+}
